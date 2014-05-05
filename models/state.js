148500d271a33922a71f99d53cc71b80df7a80bf
David Schmidt <schmdt628@gmail.com>
2014-05-05 18:14:28 -0400

    include in update

---
+++ models/state.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+exports.statelist = function statelist(state, callback){
+    var statedata = mongoose.model('statedata');
+    statedata.find({'state':state}, function (err, state){
+        if(err) {
+            console.log(err);
+        }else {
+            console.log(state);
+            callback("", state);
+        }
+
+    })
+};
